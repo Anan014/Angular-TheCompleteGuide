2e124c91382956a6df9137516310b4d7142a81ed
Anan <ananamer95@gmail.com>
2022-06-25 15:49:24 +0300

    Two-Way_databinding

--- Section 02 - The Basics/my-first-app/src/app/app.module.ts
+++ Section 02 - The Basics/my-first-app/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModel } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; 
+
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -12,7 +15,9 @@ import { ServersComponent } from './servers/servers.component';
     ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // NgModel,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
